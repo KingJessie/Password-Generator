8691bc794ad71c5a7cad51b839aa09a812e4532c
kingjessie <jessie.kinganga@gmail.com>
2023-01-12 20:15:40 +0000

    bg color changed

--- script.js
+++ script.js
@@ -1,102 +1,100 @@
 // Array of special characters to be included in password
 var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 
 // Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Array of lowercase characters to be included in password
 var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
 
 // Array of uppercase characters to be included in password
 var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
 
 // Variables to querySelector eleents from HTML file. # selects elements by their 'id' attribute.
 let pLength = document.querySelector("#p-length");
 let pUpperCase = document.querySelector("#p-uppercase");
-let pLowerCase= document.querySelector("#p-lowercase");
+let pLowerCase = document.querySelector("#p-lowercase");
 let pNumbers = document.querySelector("#p-numbers");
 let pSymbols = document.querySelector("#p-symbols");
 let pGenerate = document.querySelector("#generate");
-
-
 
 // Function for getting a random element from an array
 function getRandom(arr) {
@@ -115,19 +113,19 @@ function generatePassword() {
   // Empty array to store the character sets used to generate password.
   let initialPassword = [];
 
-  // Checks the condition of the password setting using 'if' statements, concatenating the empty array with the characters 'if' 
+  // Checks the condition of the password setting using 'if' statements, concatenating the empty array with the characters 'if'
   // password setting option (characters) is selected.
-  if(uppercase) initialPassword = initialPassword.concat(upperCasedCharacters);
-  if(lowercase) initialPassword = initialPassword.concat(lowerCasedCharacters);
-  if(numbers) initialPassword = initialPassword.concat(numericCharacters);
-  if(symbols) initialPassword = initialPassword.concat(specialCharacters);
+  if (uppercase) initialPassword = initialPassword.concat(upperCasedCharacters);
+  if (lowercase) initialPassword = initialPassword.concat(lowerCasedCharacters);
+  if (numbers) initialPassword = initialPassword.concat(numericCharacters);
+  if (symbols) initialPassword = initialPassword.concat(specialCharacters);
 
   //Empty string to store final password.
   let generatedPassword = "";
 
   // For loop to iterate over the final password's number length (length of password).
   // The loop calls getRandom() function with initialPassword passed as the argument.
-  // getRandom() randomly selects characters from initialPassword array 
+  // getRandom() randomly selects characters from initialPassword array
   // which is added to the generatedPassword to return the final password characters.
   for (let i = 0; i < length; i++) {
     generatedPassword += getRandom(initialPassword);
@@ -135,18 +133,16 @@ function generatePassword() {
   return generatedPassword;
 }
 
-
 // Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
-
+generateBtn.addEventListener("click", writePassword);
